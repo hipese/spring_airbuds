52d260013cdd96210498bee2c54dcd0293b6ec20
hipes <ansj2916@gmail.com>
2023-12-12 10:00:47 +0900

    수정

---
+++ src/main/java/com/kdt/repositories/QnaFileRepository.java
@@ -0,0 +1,14 @@
+package com.kdt.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.kdt.domain.entity.QnaAnswer;
+import com.kdt.domain.entity.QnaFile;
+
+public interface QnaFileRepository extends JpaRepository<QnaFile, Long>{
+	@Query("select q from QnaFile q where q.parentSeq = ?1%")
+	List<QnaFile> selectAllByParentSeq(Long seq);
+}
