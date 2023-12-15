b6c28044ccb80b3735d3f43977d9267faf198e6d
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-12-15 13:55:08 +0900

    dashboard report function added

--- src/main/java/com/kdt/domain/entity/MusicLike.java
+++ src/main/java/com/kdt/domain/entity/MusicLike.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 public class MusicLike {
@@ -19,6 +20,7 @@ public class MusicLike {
 	
 	@Column(name = "id")
 	private String Id;
+		
 
 	public MusicLike() {
 		super();
