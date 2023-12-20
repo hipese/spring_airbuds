db349d533107d8f465ffdc4f7bd44425d2eaafd1
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-12-20 09:20:53 +0900

    visit count function added

--- src/main/java/com/kdt/controllers/LikenFollowController.java
+++ src/main/java/com/kdt/controllers/LikenFollowController.java
@@ -1,5 +1,6 @@
 package com.kdt.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.security.Principal;
 import java.util.List;
@@ -88,6 +89,12 @@ public class LikenFollowController {
 	@GetMapping("/follwingData/{memberId}")
 	public ResponseEntity<List<HashMap<String, Object>>> getFollowingData(@PathVariable String memberId){
 		List<HashMap<String, Object>> list = fService.getMyFollow(memberId);
+//		List<FollowingSingerViewDTO> filteredList = new ArrayList<>();
+//		for(FollowingSingerViewDTO dto : list) {
+//			if(dto.getMemberId().equals(memberId)) {
+//				filteredList.add(dto);
+//			}
+//		}
 		return ResponseEntity.ok(list);
 	}
 }
--- src/main/java/com/kdt/repositories/ReportRepository.java
+++ src/main/java/com/kdt/repositories/ReportRepository.java
@@ -13,4 +13,8 @@ public interface ReportRepository extends JpaRepository<Report, Long>{
 	// 신고 월별 데이터 산출
 	@Query("SELECT function('YEAR',r.reportWriteDate) AS year, function('MONTH',r.reportWriteDate) AS month, function('COUNT',r) AS count FROM Report r GROUP BY function('YEAR',r.reportWriteDate), function('MONTH',r.reportWriteDate) ORDER BY function('YEAR',r.reportWriteDate), function('MONTH',r.reportWriteDate)")
 	public List<Map<String, Object>> selectReportByMonth();
+	
+	
+	@Query("SELECT function('COUNT', r) FROM Report r WHERE function('DAY',r.reportWriteDate) = function('DAY',function('CURDATE')) AND function('MONTH',r.reportWriteDate) = function('MONTH',function('CURDATE')) AND function('YEAR',r.reportWriteDate) = function('YEAR',function('CURDATE'))")
+	public Long selectReportCountByDay();
 }
