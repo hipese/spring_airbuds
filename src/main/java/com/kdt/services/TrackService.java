4c57b77153fc1a8ef3d2e2741a868b98acd8169c
hipes <ansj2916@gmail.com>
2023-12-29 18:57:26 +0900

    파일 업로드 방식 변경

--- src/main/java/com/kdt/services/TrackService.java
+++ src/main/java/com/kdt/services/TrackService.java
@@ -18,6 +18,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.kdt.domain.entity.LikeTrackView;
 import com.kdt.domain.entity.MusicTags;
 import com.kdt.domain.entity.Track;
@@ -61,6 +66,10 @@ public class TrackService {
 	@Autowired
 	private LikeTrackViewRepository ltvRepo;
 	
+	private final Storage storage = StorageOptions.getDefaultInstance().getService();
+	private final String bucketName = "an_airbuds";
+
+	
 	private static final Logger logger=LoggerFactory.getLogger(TrackService.class); 
 
 	@Transactional
@@ -76,6 +85,8 @@ public class TrackService {
 			throws Exception {
 		
 		Track savedTrack = null;
+		
+		
 		
 		// 음원 파일 저장
 		File uploadPath = new File("c:/tracks");
@@ -93,7 +104,15 @@ public class TrackService {
 			MultipartFile file = files;
 			
 			String filename = file.getOriginalFilename();
-			String sys_filename = UUID.randomUUID() + filename;
+			String sys_filename = UUID.randomUUID()+"_" + filename;
+			
+			
+			BlobId blobId1 = BlobId.of(bucketName, "/track/"+sys_filename);//경로이름 지정한 장소 
+	        BlobInfo blobInfo1 = BlobInfo.newBuilder(blobId1).build();
+
+	        // 파일을 GCS에 업로드하고 Blob 객체를 받습니다.
+	        Blob blob1 = storage.create(blobInfo1, file.getBytes());
+	        
 			
 			// 문자열을 double로 변환하고 long으로 반올림
 			double durationDouble = Double.parseDouble(durations);
@@ -106,7 +125,7 @@ public class TrackService {
 			dto.setTitle(name);
 			dto.setAlbumId(null);
 			dto.setDuration(durationTime);
-			dto.setFilePath(sys_filename);
+			dto.setFilePath(blob1.getMediaLink());
 			dto.setTrackNumber(0L);
 			dto.setViewCount(0L);
 			dto.setWriter(writer);
@@ -143,6 +162,15 @@ public class TrackService {
 	        MultipartFile imgFile = imagefile;
 	        String imageName = imgFile.getOriginalFilename();
 	        String sys_imageName = UUID.randomUUID().toString() + "_" + imageName;
+	        
+	        BlobId blobId2 = BlobId.of(bucketName, "/tracks/image"+sys_imageName);//경로이름 지정한 장소 
+	        BlobInfo blobInfo2 = BlobInfo.newBuilder(blobId2).build();
+
+	        Blob blob2 = storage.create(blobInfo2, imgFile.getBytes());
+	        
+	        
+	        System.out.println(blob2.getMediaLink());
+	        
 	        File destImageFile = new File(imagePath, sys_imageName);
 	        imgFile.transferTo(destImageFile);
 
@@ -151,7 +179,7 @@ public class TrackService {
 	        
 	        TrackImageDTO imagedto=new TrackImageDTO();
 	        imagedto.setTrackId(savedTrack.getTrackId());
-	        imagedto.setImagePath(sys_imageName);
+	        imagedto.setImagePath(blob2.getMediaLink());
 	        imageRepo.save(imageMapper.toEntity(imagedto));
 	    }
 	    
@@ -322,42 +350,17 @@ public class TrackService {
 		
 		
 		if (entity != null) {
-			
 			//track_id과 일치하는 테그를 전부 삭제한다.
 			tagRepo.deleteAllByTrackTagTrackId(track_id);
-			
-			
-//			경로에 이미지가 있으면 삭제하고 데이터베이스에 값을 지운다.
-			String imagePath="c:/tracks/image"+ File.separator+imageEntity.getImagePath();
-			File imageToDelete = new File(imagePath);
-			
-			if (imageToDelete.exists()) {
-				
-				if(entity.getAlbumId()==null) {
-					imageToDelete.delete();
-				}
-				imageRepo.deleteById(track_id);// 데이터베이스에서 이미지 삭제
-				
-			} else {
-				logger.error("파일이 존재하지 않습니다: " + imagePath);
-			}
-			
+			imageRepo.deleteById(track_id);// 데이터베이스에서 이미지 삭제
 //			음원에 값을 확인하고 
 			String filePath = "c:/tracks" + File.separator + entity.getFilePath();
 			File fileToDelete = new File(filePath);
 			
-
-			if (fileToDelete.exists()) {
-				fileToDelete.delete();
-				tRepo.deleteById(track_id); // 데이터베이스에서 삭제
+			tRepo.deleteById(track_id); // 데이터베이스에서 삭제
 				
-			} else {
-				logger.error("파일이 존재하지 않습니다: " + filePath);
-			}
-		} else {
-			logger.error("파일이 존재하지 않습니다: " + track_id);
-		}
 	}
+}
 
 	// 초를 HH:mm:ss 형식의 문자열로 변환 (time 형태로 변환)
 	private String convertSecondsToTimeString(long seconds) {
